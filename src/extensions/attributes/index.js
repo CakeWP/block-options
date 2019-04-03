74400fb7b9256b2af563393e3ba50f52dfd69033
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-04-03 19:50:21 +0800

    Add unique attributes and classes

--- src/extensions/attributes/index.js
+++ src/extensions/attributes/index.js
@@ -1,6 +1,15 @@
+/**
+ * External Dependencies
+ */
+import classnames from 'classnames';
 
+/**
+ * WordPress Dependencies
+ */
 const { __ } = wp.i18n;
 const { addFilter } = wp.hooks; 
+const { Fragment } = wp.element; 
+const { createHigherOrderComponent } = wp.compose; 
 
 /**
  * Filters registered block settings, extending attributes with anchor using ID
@@ -13,6 +22,8 @@ const { addFilter } = wp.hooks;
 function addAttributes( settings ) {
 	
 	if( typeof settings.attributes !== 'undefined' ){
+		let d = new Date();
+
 		settings.attributes = Object.assign( settings.attributes, {
 			editorskit:{ 
 				type: 'object',
@@ -31,10 +42,94 @@ function addAttributes( settings ) {
 	return settings;
 }
 
+/**
+ * Add custom EditorsKit attributes to selected blocks
+ *
+ * @param {function|Component} BlockEdit Original component.
+ * @return {string} Wrapped component.
+ */
+const withAttributes = createHigherOrderComponent( ( BlockEdit ) => {
+	return ( props ) => {
+
+		const {
+			clientId,
+			attributes,
+			setAttributes,
+		} = props;
+
+		if ( typeof attributes.editorskit === 'undefined' ) {
+			attributes.editorskit = [];
+		}
+
+		//add unique selector
+		if( typeof attributes.editorskit.id === 'undefined' ){
+			let d = new Date();
+
+			const editorskit = Object.assign( { id: "editorskit-" + d.getMonth() + d.getDate() + d.getHours() + d.getMinutes() + d.getSeconds() + d.getMilliseconds() }, attributes.editorskit );
+			setAttributes( { editorskit: editorskit } );
+		}
+
+		return (
+			<Fragment>
+				<BlockEdit {...props} />
+			</Fragment>
+		);
+	};
+}, 'withAttributes');
+
+/**
+ * Override props assigned to save component to inject atttributes
+ *
+ * @param {Object} extraProps Additional props applied to save element.
+ * @param {Object} blockType  Block type.
+ * @param {Object} attributes Current block attributes.
+ *
+ * @return {Object} Filtered props applied to save element.
+ */
+function applyExtraClass(extraProps, blockType, attributes) {
+
+	const { editorskit } = attributes;
+	
+	if ( typeof editorskit !== 'undefined' ) {
+
+		if( typeof editorskit.id !== 'undefined' ){
+			extraProps.className = classnames( extraProps.className, editorskit.id );
+		}
+
+		if( typeof editorskit.desktop !== 'undefined' && ! editorskit.desktop ){
+			extraProps.className = classnames( extraProps.className, 'editorskit-no-desktop' );
+		}
+
+		if( typeof editorskit.tablet !== 'undefined' && ! editorskit.tablet ){
+			extraProps.className = classnames( extraProps.className, 'editorskit-no-tablet' );
+		}
+
+		if( typeof editorskit.mobile !== 'undefined' && ! editorskit.mobile ){
+			extraProps.className = classnames( extraProps.className, 'editorskit-no-mobile' );
+		}
+		
+	}
+
+	console.log( editorskit );
+
+	return extraProps;
+}
 
 
 addFilter(
 	'blocks.registerBlockType',
 	'editorskit/custom/attributes',
 	addAttributes
+);
+
+addFilter(
+	'editor.BlockEdit',
+	'editorskit/attributes',
+	withAttributes
+);
+
+addFilter(
+	'blocks.getSaveContent.extraProps',
+	'editorskit/applyExtraClass',
+	applyExtraClass
 );
