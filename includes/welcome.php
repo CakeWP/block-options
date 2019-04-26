c94fc26ec4d7a2ec8282fb48591759f708855392
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-04-26 20:11:42 +0800

    Improve the welcome page

--- includes/welcome.php
+++ includes/welcome.php
@@ -44,13 +44,34 @@ if( !class_exists( 'BLOCKOPS_Welcome' ) ){
 		function welcome_content(){ ?>
 			<div class="wrap about-wrap editorskit-about-wrap">
 				<div class="getting-started__content">
-					<h1><?php echo esc_html__( 'Thanks for choosing EditorsKit, the page building block options for the new WordPress block editor.', 'editorskit' ); ?></h1>
+					<h1><?php echo esc_html__( 'Get started with EditorsKit.', 'editorskit' ); ?></h1>
+					
+					<p><strong><?php echo esc_html__( 'Thank you for choosing EditorsKit!', 'editorskit' ); ?></strong></p>
+					
+					<p><?php echo esc_html__( 'You have just enabled set of useful tools that will help you manage each blocks and improve your content workflow with the new editor.', 'editorskit' ); ?></p>
+					
+					<p><strong><?php echo esc_html__( 'Here is the video on how the plugin works:', 'editorskit' ); ?></strong></p>
+
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/_Im6fuUZXfc" frameborder="0" allowfullscreen></iframe>
+
+					<p>
+						<?php
+						echo sprintf(
+							esc_html__( 'If you have any questions or suggestion, let us know through %1$sTwitter%4$s or our %2$sFacebook community %4$s. Also, %3$ssubscribe to our newsletter%4$s if you want to stay up to date with what\'s new and upcoming at EditorsKit.', '@@textdomain' ), '<a href="https://twitter.com/editorskit" target="_blank">', '<a href="https://www.facebook.com/groups/1306393256173179/" target="_blank">', '<a href="https://editorskit.com/" target="_blank">', '</a>'
+						);
+						?>
+					</p>
+
+					<p><?php echo esc_html__( 'Happy building!', 'editorskit' ); ?></p>
+
+					<p><img src="<?php echo esc_url( EDITORSKIT_PLUGIN_URL . 'dist/images/logo-800.png' ); ?>" alt="<?php echo esc_attr__( 'EditorsKit Team', 'editorskit' ); ?>"></p>
+
 				</div>
 			</div>
 		<?php }
 
 		function redirect($plugin){
-			if($plugin=='block-options/plugin.php') {
+			if( $plugin == 'block-options/plugin.php' || $plugin=='editorskit/plugin.php' ) {
 				wp_redirect(admin_url('index.php?page=editorskit-getting-started'));
 				die();
 			}
