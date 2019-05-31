7eec135a38e5931c85f6b3322e624ed8fa2264ba
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-05-31 16:05:38 +0800

    Move visibility settings to own folder under /block-settings

--- src/extensions/block-settings/index.js
+++ src/extensions/block-settings/index.js
@@ -1,15 +1,4 @@
 /**
  * Internal dependencies
  */
-import './styles/editor.scss';
-import BlockSettings from './components/modal';
-
-/**
- * WordPress dependencies
- */
-const { registerPlugin } = wp.plugins;
-
-registerPlugin( 'editorskit-block-settings', {
-    icon: 'visibility',
-	render: BlockSettings,
-} );
+import './visibility-settings';
