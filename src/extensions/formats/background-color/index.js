9bf801678937b9ec2cb5255b6202e5726a738c69
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-06-15 13:40:19 +0800

    Use <span> instead of <mark> to avoid CSS color conflict

--- src/extensions/formats/background-color/index.js
+++ src/extensions/formats/background-color/index.js
@@ -27,7 +27,7 @@ const name = 'editorskit/background';
 export const backgroundColor = {
 	name,
 	title: __( 'Background Color' ),
-	tagName: 'mark',
+	tagName: 'span',
 	className: 'has-inline-background',
 	attributes: {
 		style: 'style',
