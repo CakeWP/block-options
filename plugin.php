c4d19d3d72e950f32479e3b082333299078dbc42
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-11-01 14:50:21 +0800

    More wpcs fixes and improvements

--- plugin.php
+++ plugin.php
@@ -128,7 +128,7 @@ if ( ! class_exists( 'EditorsKit' ) ) :
 			require_once EDITORSKIT_PLUGIN_DIR . 'includes/class-editorskit-block-assets.php';
 			require_once EDITORSKIT_PLUGIN_DIR . 'includes/class-render-block.php';
 			require_once EDITORSKIT_PLUGIN_DIR . 'includes/class-acf-support.php';
-			require_once EDITORSKIT_PLUGIN_DIR . 'includes/class-features-manager.php';
+			require_once EDITORSKIT_PLUGIN_DIR . 'includes/class-editorskit-features-manager.php';
 			require_once EDITORSKIT_PLUGIN_DIR . 'includes/class-post-meta.php';
 			require_once EDITORSKIT_PLUGIN_DIR . 'includes/function-hide-title.php';
 			require_once EDITORSKIT_PLUGIN_DIR . 'includes/class-custom-css-classes.php';
