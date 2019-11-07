0b7a1ec2509947404480477c3491b9642dcbdf5b
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-11-07 20:29:49 +0800

    wpcs fixes

--- includes/class-editorskit-block-assets.php
+++ includes/class-editorskit-block-assets.php
@@ -122,13 +122,21 @@ class EditorsKit_Block_Assets {
 			false
 		);
 
-		wp_enqueue_script(
-			$this->slug . '-devtools',
-			$this->url . '/build/devtools.js',
-			array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-plugins', 'wp-components', 'wp-edit-post', 'wp-api', 'wp-editor', 'wp-hooks', 'lodash' ),
-			time(),
-			false
-		);
+		if ( current_theme_supports( 'editorskit-devtools' ) ) {
+
+			$theme_support = get_theme_support( 'editorskit-devtools' );
+
+			if ( $theme_support ) {
+
+				wp_enqueue_script(
+					$this->slug . '-devtools',
+					$this->url . '/build/devtools.js',
+					array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-plugins', 'wp-components', 'wp-edit-post', 'wp-api', 'wp-editor', 'wp-hooks', 'lodash' ),
+					time(),
+					false
+				);
+			}
+		}
 
 		$version = '';
 		$is_core = true;
