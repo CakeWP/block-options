f5048c17618b4ea5aa8958a0109d56bb1fc49804
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-11-08 13:39:00 +0800

    Use outline instead of border

--- build/editor.asset.php
+++ build/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'a0f5af462b432dec3fa834af8c680ba7');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'a4ab47ac79dba7ea175132946fab93a0');
