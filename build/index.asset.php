d5f750e81bfdcde17a1505b345bedee6ba6914ab
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2020-02-27 13:10:35 +0800

    Prevent error when switching to Code Editor using Keyboard shortcut

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-element', 'wp-polyfill', 'wp-wordcount'), 'version' => '40b234aa0b1d7aca65a88c99111bf754');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-element', 'wp-polyfill', 'wp-wordcount'), 'version' => '261ebacbb8343d72ee5aead2da0ee3e0');
