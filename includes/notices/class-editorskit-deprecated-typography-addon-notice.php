9fdcbfe3239e2a9f448f86851f8a2475cdc5ad56
farhan-shafi <farhanshafi34@gmail.com>
2022-09-01 15:56:35 +0500

    remove hyphen

--- includes/notices/class-editorskit-deprecated-typography-addon-notice.php
+++ includes/notices/class-editorskit-deprecated-typography-addon-notice.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Notice that displays deprecation notice for typography addon
  *
@@ -7,11 +8,12 @@
  */
 
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
-class Editorskit_Deprecated_Typography_Addon_Notice {
+class Editorskit_Deprecated_Typography_Addon_Notice
+{
 
 	public static $key         = 'ek-deprecated-typography-addon-notice';
 	public static $dismiss_key = 'dismiss-ek-typography-addon-notice';
@@ -21,10 +23,11 @@ class Editorskit_Deprecated_Typography_Addon_Notice {
 	 *
 	 * @return void
 	 */
-	public function __construct() {
-		add_action( 'init', array( $this, 'register' ), 7 );
-		add_action( 'admin_notices', array( $this, 'render' ) );
-		add_action( 'init', array( $this, 'handle_dismiss' ), 10 );
+	public function __construct()
+	{
+		add_action('init', array($this, 'register'), 7);
+		add_action('admin_notices', array($this, 'render'));
+		add_action('init', array($this, 'handle_dismiss'), 10);
 	}
 
 	/**
@@ -32,7 +35,8 @@ class Editorskit_Deprecated_Typography_Addon_Notice {
 	 *
 	 * @return void
 	 */
-	public function register() {
+	public function register()
+	{
 		register_setting(
 			'editorskit',
 			static::$key,
@@ -46,10 +50,11 @@ class Editorskit_Deprecated_Typography_Addon_Notice {
 		);
 	}
 
-	public function handle_dismiss() {
+	public function handle_dismiss()
+	{
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
-		if ( isset( $_GET[ static::$dismiss_key ] ) ) {
-			update_option( static::$key, 'hidden' );
+		if (isset($_GET[static::$dismiss_key])) {
+			update_option(static::$key, 'hidden');
 		}
 	}
 
@@ -58,8 +63,9 @@ class Editorskit_Deprecated_Typography_Addon_Notice {
 	 *
 	 * @return bool - True if should render the notice, otherwise false.
 	 */
-	public function should_render() {
-		return 'hidden' !== get_option( static::$key );
+	public function should_render()
+	{
+		return 'hidden' !== get_option(static::$key);
 	}
 
 	/**
@@ -67,36 +73,34 @@ class Editorskit_Deprecated_Typography_Addon_Notice {
 	 *
 	 * @return void
 	 */
-	public function render() {
+	public function render()
+	{
 
-		if ( ! $this->should_render() ) {
+		if (!$this->should_render()) {
 			return;
 		}
 
-		?>
+?>
 		<div class="editorskit_activated_notice notice-error notice is-dismissible" style="box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);">
-		   <p>
+			<p>
 				<?php
-					echo sprintf(
-						esc_html__( '%1$sEditorsKit Plugin%2$s typography has been deprecated. If you want to continue using %1$s%3$sClick here to install the Typography Add-on%4$s%2$s.', 'editorskit-typography-addon' ),
-						'<strong>',
-						'</strong>',
-						'<a target="_blank" href="https://github.com/CakeWP/deprecated-editorskit-typography-addon/releases">',
-						'</a>'
-					);
+				echo sprintf(
+					esc_html__('%1$sEditorsKit Plugin%2$s typography has been deprecated. If you want to continue using %1$s%3$sClick here to install the Typography Addon%4$s%2$s.', 'editorskit-typography-addon'),
+					'<strong>',
+					'</strong>',
+					'<a target="_blank" href="https://github.com/CakeWP/deprecated-editorskit-typography-addon/releases">',
+					'</a>'
+				);
 				?>
-		   </p>
-		   <a style="text-decoration:none" class="notice-dismiss" href="?<?php echo static::$dismiss_key; ?>">
+			</p>
+			<a style="text-decoration:none" class="notice-dismiss" href="?<?php echo static::$dismiss_key; ?>">
 				<span class="screen-reader-text">
-					<?php esc_html_x( 'Dismiss this notice.', 'Dismiss unsupported version notice screen reader.', 'block-slider' ); ?>
+					<?php esc_html_x('Dismiss this notice.', 'Dismiss unsupported version notice screen reader.', 'block-slider'); ?>
 				</span>
 			</a>
-		  </div>
-		<?php
+		</div>
+<?php
 	}
-
 }
 
 new Editorskit_Deprecated_Typography_Addon_Notice();
-
-
