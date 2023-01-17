f86706928a341cbeedb9e2e6f044a42613026aeb
Zafar Kamal <sk4915497@gmail.com>
2023-01-17 13:19:48 +0500

    :100: add notice, and plugin shortcuts

--- build/settings.asset.php
+++ build/settings.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '3bdbf4dd52b228b750b80e563589578f');
+<?php return array('dependencies' => array('lodash', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'cc618fb3290cba76db7766edacb15dba');
