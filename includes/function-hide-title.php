d04372e37d883d3c58cc456be7a67db4bb7d25a7
Farhan <farhanshafi34@gmail.com>
2023-04-08 13:52:49 +0500

    :+1: fix search not correct

--- includes/function-hide-title.php
+++ includes/function-hide-title.php
@@ -110,7 +110,7 @@ add_filter(
 	 */
 	function( $block_content, $parsed_block ) {
 
-		if ( 'core/query' !== $parsed_block['blockName'] || isset( $parsed_block['ek_rendered'] ) ) {
+		if ( 'core/query' !== $parsed_block['blockName'] || isset( $parsed_block['ek_rendered'] ) || is_search() ) {
 			return $block_content;
 		}
 
